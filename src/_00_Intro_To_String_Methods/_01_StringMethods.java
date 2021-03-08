d7b7990ad1414e21fb0b74570b67557537bb49b4
Derek <Derek@LAPTOP-T10GJCDD>
2021-03-08 13:32:58 -0800

    String methods complete

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -45,8 +45,8 @@ public class _01_StringMethods {
 	// If String s contains the word "underscores", change all of the spaces
 	// to underscores
 	public static String formatSpaces(String s) {
-		if (s.contains("underscores")) {
-			s.replace(" ", "_");
+		if (s.contains("underscores") == true) {
+			s = s.replace(" ", "_");
 		}
 		return s;
 	}
@@ -59,28 +59,32 @@ public class _01_StringMethods {
 		String[] words1 = s1.trim().split(" ");
 		String[] words2 = s2.trim().split(" ");
 		String[] words3 = s3.trim().split(" ");
-		if (words1[1].compareTo(words2[1]) >= 1) {
-			if (words1[1].compareTo(words3[1]) >= 1) {
-				return words1[1];
+		if (words1[1].compareTo(words2[1]) <= -1) {
+			if (words1[1].compareTo(words3[1]) <= -1) {
+				return s1.trim();
 			} else {
-				return words3[1];
+				return s3.trim();
 			}
 		} else {
-			if (words2[1].compareTo(words3[1]) >= 1) {
-				return words2[1];
+			if (words2[1].compareTo(words3[1]) <= -1) {
+				return s2.trim();
 			} else {
-				return words3[1];
+				return s3.trim();
 			}
 		}
 	}
 
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		int sum=0;
+		int sum = 0;
+		if (s.equals("")) {
+			return sum;
+		}
+		String Sub = "";
 		for (int i = 0; i < s.length(); i++) {
-			if (Character.isDigit(s.charAt(i))==true); {
-				sum+=s.charAt(i);
-				
+			if (Character.isDigit(s.charAt(i)) == true) {
+				Sub = "" + s.charAt(i);
+				sum += Integer.parseInt(Sub);
 			}
 		}
 		return sum;
@@ -88,38 +92,70 @@ public class _01_StringMethods {
 
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int total = 0;
+		if (s.contains(substring)) {
+			for (int i = 0; i < s.length() - substring.length() + 1; i++) {
+				if (s.substring(i, i + substring.length()).equals(substring)) {
+					total += 1;
+				}
+			}
+		}
+		return total;
 	}
 
 	// Call Utilities.encrypt at the bottom of this file to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		return Utilities.encrypt(s.getBytes(), (byte) key);
 	}
 
 	// Call Utilities.decrypt at the bottom of this file to decrypt the
 	// cyphertext (encrypted text)
 	public static String decrypt(String s, char key) {
-		return null;
+		return Utilities.decrypt(s, (byte) key);
 	}
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		int total = 0;
+		String[] words = s.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].endsWith(substring)) {
+				total += 1;
+			}
+		}
+		return total;
 	}
 
 	// Given String s, return the number of characters between the first
 	// occurrence of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+
+		return (s.lastIndexOf(substring) - substring.length()) - s.indexOf(substring);
 	}
 
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
-		return true;
+		String sub = "";
+		s.trim();
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isLetter(s.charAt(i)) == false) {
+				s = s.replace("" + s.charAt(i), "");
+			}
+			
+		}
+		
+		for (int i = s.length()-1; i > -1; i--) {
+			sub += s.charAt(i);
+		}
+		System.out.println(s+" "+sub);
+		if (sub.equalsIgnoreCase(s)) {
+			return true;
+		}
+		return false;
 	}
 }
 
