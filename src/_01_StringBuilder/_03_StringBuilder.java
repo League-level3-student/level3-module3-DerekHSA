aedeb6156e31d55c768d61c0ccd248c733f9c466
Derek <Derek@LAPTOP-T10GJCDD>
2021-03-09 16:57:50 -0800

    half completed

--- src/_01_StringBuilder/_03_StringBuilder.java
+++ src/_01_StringBuilder/_03_StringBuilder.java
@@ -2,20 +2,28 @@ package _01_StringBuilder;
 
 
 public class _03_StringBuilder {
-    
+    static StringBuilder builder;
     public static String append(String str, char[] characters) {
-        return null;
+    	builder = new StringBuilder(str);
+    	builder.append(characters);
+        return builder.toString();
     }
     
     public static String reverse(String str) {
-        return null;
+    	builder = new StringBuilder(str);
+    	builder.reverse();
+        return builder.toString();
     }
     
     public static String insert(String str, int index, char newChar) {
-        return null;
+    	builder = new StringBuilder(str);
+    	builder.insert(index, newChar);
+        return builder.toString();
     }
     
     public static String delete(String str, int startIndex, int endIndex) {
-        return null;
+    	builder = new StringBuilder(str);
+    	builder.delete(startIndex, endIndex);
+        return builder.toString();
     }
 }
--- src/_02_The_Wave/_01_TheWave.java
+++ src/_02_The_Wave/_01_TheWave.java
@@ -3,24 +3,41 @@ package _02_The_Wave;
 import java.util.ArrayList;
 
 public class _01_TheWave {
-    /*
-     * Background:
-     * https://en.wikipedia.org/wiki/Wave_%28audience%29 
-     * 
-     * Task:
-     * Your task is to create a function that turns a string into a Wave.
-     * You will be passed a string and you must return that string in an
-     * ArrayList where an uppercase letter is a person standing up.
-     * Example:
-     * wave("hello") => "Hello", "hEllo", "heLlo", "helLo", "hellO"
-     * 
-     * 1. The input string will always be lower case but maybe empty.
-     * 2. If the character in the string is whitespace then pass over it as
-     *    if it was an empty seat.
-     */
-    
-    public static ArrayList<String> wave(String str) {
-        
-        return null;
-    }
+	/*
+	 * Background: https://en.wikipedia.org/wiki/Wave_%28audience%29
+	 * 
+	 * Task: Your task is to create a function that turns a string into a Wave. You
+	 * will be passed a string and you must return that string in an ArrayList where
+	 * an uppercase letter is a person standing up. Example: wave("hello") =>
+	 * "Hello", "hEllo", "heLlo", "helLo", "hellO"
+	 * 
+	 * 1. The input string will always be lower case but maybe empty. 2. If the
+	 * character in the string is whitespace then pass over it as if it was an empty
+	 * seat.
+	 */
+
+	public static ArrayList<String> wave(String str) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		for (int i = 0; i < str.length(); i++) {
+			String temp = "";
+			for (int j = 0; j < str.length(); j++) {
+				if (j==i) {
+					temp += Character.toUpperCase(str.charAt(j));
+				} else {
+					temp += str.charAt(j);
+				}
+				
+			}
+			if (Character.isLetter(str.charAt(i))==true) {
+				list.add(temp);
+			}
+			
+
+		}
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		return list;
+	}
 }
